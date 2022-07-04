9df8f2de6f946cd9289b296c781be45386be05ec
kawash <kawashhabiby@hotmail.com>
2022-07-04 20:14:17 +0500

    c

--- components/Post.js
+++ components/Post.js
@@ -1,22 +1,16 @@
-import React from 'react'
 
-const post=[{name:'name',descr:'',message:'hello',photoUrl:''},
-{name:'name',descr:'',message:'hello',photoUrl:''},
-{name:'name',descr:'',message:'hello',photoUrl:''},
-{name:'name',descr:'',message:'hello',photoUrl:''}
-,{name:'name',descr:'',message:'hello',photoUrl:''}]
-
-
-
-
-
-
-
+import styles from "/styles/Post.module.css"
 const Post = ({name,descr,message,photoUrl}) => {
   return (
     <>
-        <div className="post">
-            <div className="post__header"></div>
+        <div className={styles.post}>
+            <div className={styles.post__header}>
+            <img
+            src="https://media-exp2.licdn.com/dms/image/C4D03AQFUWjDlOD8vXQ/profile-displayphoto-shrink_100_100/0/1612563234794?e=1661990400&v=beta&t=WvKeZnMoBBM2OxaC3o-NTlZUZpNeqCm6vKooQwDYSI4"
+            alt="avatar"
+            className="w-[4rem] h-[4rem] shadow-md rounded-full shadow-gray-400 bg-slate-200"
+          />
+            </div>
             <div className="post__info"></div>
             <div className="post__body"></div>        
         </div>    
